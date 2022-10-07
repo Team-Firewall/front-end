4b98c4cce22850a9ca9ae3bd518e025df0362976
jinhyo-dev <jinhyo0850@gmail.com>
2022-10-08 01:46:22 +0900

    login func add

--- src/Components/LoginContainer.tsx
+++ src/Components/LoginContainer.tsx
@@ -10,6 +10,8 @@ import classNames from "classnames/bind"
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import styles from '../Style/Login.module.css'
 import { AiFillExclamationCircle } from 'react-icons/ai'
+import jwt_decode from "jwt-decode";
+import axios, { AxiosResponse } from "axios";
 
 const cs = classNames.bind(styles)
 
@@ -22,7 +24,34 @@ interface State {
 }
 
 export default function InputAdornments() {
-  const [values, setValues] = React.useState<State>({
+
+  const login = () => {
+    let data = {
+      "account": idValues.password,
+      "password": passwordValues.password
+    }
+
+    axios.post('http://localhost:3001/sign/in', JSON.stringify(data), {
+      headers: {"Content-Type": "application/json"}
+    }).then((res: any) => {
+      if (res.data.success) {
+        let token: any = jwt_decode(res.data.token)
+        alert(token.name + '님 로그인 테스트 성공')
+      }
+    })
+
+
+  }
+
+  const [passwordValues, setPasswordValues] = React.useState<State>({
+    amount: '',
+    password: '',
+    weight: '',
+    weightRange: '',
+    showPassword: false,
+  });
+
+  const [idValues, setIdValues] = React.useState<State>({
     amount: '',
     password: '',
     weight: '',
@@ -32,13 +61,18 @@ export default function InputAdornments() {
 
   const handleChange =
     (prop: keyof State) => (event: React.ChangeEvent<HTMLInputElement>) => {
-      setValues({ ...values, [prop]: event.target.value });
+      setPasswordValues({...passwordValues, [prop]: event.target.value});
+    };
+
+  const handleChangeID =
+    (prop: keyof State) => (event: React.ChangeEvent<HTMLInputElement>) => {
+      setIdValues({...idValues, [prop]: event.target.value});
     };
 
   const handleClickShowPassword = () => {
-    setValues({
-      ...values,
-      showPassword: !values.showPassword,
+    setPasswordValues({
+      ...passwordValues,
+      showPassword: !passwordValues.showPassword,
     });
   };
 
@@ -50,7 +84,8 @@ export default function InputAdornments() {
     <Box>
       <div className={cs('input-container')}>
 
-        <FormControl sx={{ m: 1, width: '350px' }} variant="outlined">
+        <FormControl sx={{m: 1, width: '350px'}} variant="outlined"
+                     onChange={handleChangeID('password')}>
           <InputLabel htmlFor="outlined-adornment-password">학번</InputLabel>
           <OutlinedInput
             label="학번"
@@ -58,16 +93,16 @@ export default function InputAdornments() {
         </FormControl>
 
         <div className={cs('exclamation-phrases')}>
-          <AiFillExclamationCircle style={{ marginBottom: '-2.5px', color: '#f00', fontSize: '14px' }}/>
+          <AiFillExclamationCircle style={{marginBottom: '-2.5px', color: '#f00', fontSize: '14px'}}/>
           <span>아이디 또는 학번이 기억나지 않는다면 선생님께 문의해주세요.</span>
         </div>
 
-        <FormControl sx={{ m: 1, width: '350px', marginTop: '25px' }} variant="outlined">
-          <InputLabel htmlFor="outlined-adornment-password">비밀번호</InputLabel>
+        <FormControl sx={{m: 1, width: '350px', marginTop: '25px'}} variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-password" >비밀번호</InputLabel>
           <OutlinedInput
             id="outlined-adornment-password"
-            type={values.showPassword ? 'text' : 'password'}
-            value={values.password}
+            type={passwordValues.showPassword ? 'text' : 'password'}
+            value={passwordValues.password}
             onChange={handleChange('password')}
             endAdornment={
               <InputAdornment position="end">
@@ -77,18 +112,19 @@ export default function InputAdornments() {
                   onMouseDown={handleMouseDownPassword}
                   edge="end"
                 >
-                  {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                  {passwordValues.showPassword ? <VisibilityOff/> : <Visibility/>}
                 </IconButton>
               </InputAdornment>
             }
             label="Password"
           />
         </FormControl>
+
         <div className={cs('exclamation-phrases')}>
-          <span style={{ color: '#f00' }}>학번 또는 비밀번호가 올바르지 않습니다.</span>
+          <span style={{color: '#f00'}}>학번 또는 비밀번호가 올바르지 않습니다.</span>
         </div>
 
-        <button className={cs('login-btn')}>로그인</button>
+        <button className={cs('login-btn')} onClick={login}>로그인</button>
 
       </div>
     </Box>
