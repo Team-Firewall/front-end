9295d734962066b406ea7ef1342be5e59a935deb
jinhyo-dev <jinhyo0850@gmail.com>
2022-11-16 01:30:23 +0900

    Add User design done

--- src/Components/AddUserComponents/AddUserWithTyping.tsx
+++ src/Components/AddUserComponents/AddUserWithTyping.tsx
@@ -6,8 +6,8 @@ import { AnimatePresence, motion, useIsPresent } from "framer-motion";
 import classNames from "classnames/bind";
 import styles from '../../Style/AddUser/AddUserWithTyping.module.css'
 import { FiUserPlus } from 'react-icons/fi'
-import { AiOutlineCheck } from 'react-icons/ai'
 import { CgPlayListAdd, CgPlayListRemove } from 'react-icons/cg'
+import { AiFillEye, AiFillEyeInvisible, AiOutlineCheck } from 'react-icons/ai'
 
 const cs = classNames.bind(styles)
 
@@ -25,16 +25,10 @@ const AddUserWithTyping = () => {
     parentsPhoneNum2: string,
     account: string,
     password: string,
-    showPassword: boolean
   }
 
-  const [newUserName, setNewUserName] = useState<string>()
-  const [newPhoneNumber, setNewPhoneNumber] = useState<any>()
-  const [newUserId, setNewUserId] = useState<string>()
-  const [newUserPassword, setNewUserPassword] = useState<string>()
-  const [newUserPosition, setNewUserPosition] = useState<number>()
-  const [items, setItems] = useState([1, 2, 3]);
   const [newUserArray, setNewUserArray] = useState<NewUser[]>([])
+  const [showPassword, setShowPassword] = useState<boolean>(false)
 
   const isPresent = useIsPresent();
 
@@ -53,7 +47,6 @@ const AddUserWithTyping = () => {
         parentsPhoneNum2: '',
         account: '',
         password: '',
-        showPassword: false
       }]))
   }
 
@@ -125,12 +118,16 @@ const AddUserWithTyping = () => {
         } : it
       ))
     } else if (classification === 'password') {
-      setNewUserArray(newUserArray.map((it) =>
-        it.index === idx ? {
-          ...it,
-          password: value,
-        } : it
-      ))
+      if (showPassword) {
+        setShowPassword(false)
+      } else {
+        setNewUserArray(newUserArray.map((it) =>
+          it.index === idx ? {
+            ...it,
+            password: value,
+          } : it
+        ))
+      }
     }
     console.log(newUserArray)
   }
@@ -185,21 +182,17 @@ const AddUserWithTyping = () => {
     //   .catch(() => alert('ERROR'))
   }
 
-  const handleShowPassword = (idx: number) => {
-    setNewUserArray(newUserArray.map((it) =>
-      it.index === idx ? {
-        ...it,
-        showPassword: !newUserArray[idx].showPassword,
-      } : it
-    ))
+  const handleShowPassword = () => {
+    setShowPassword(!showPassword)
   }
+
 
   return (
     <div>
       <div className={'add-user-container'}
            style={{width: newUserArray.length < 1 ? '80%' : '100%', transition: '0.5s'}}>
 
-        <div className={cs('table-container')} style={{ display: newUserArray.length === 0 ? 'table' : '' }}>
+        <div className={cs('table-container')} style={{display: newUserArray.length === 0 ? 'table' : ''}}>
 
           {
             newUserArray.length === 0 && (
@@ -225,16 +218,16 @@ const AddUserWithTyping = () => {
               <table cellPadding={0} className={cs('user-table')}>
                 <thead>
                 <tr>
-                  <th style={{ borderTopLeftRadius: '5px', width: '120px' }}>권한</th>
-                  <th style={{ width: '50px' }}>학년</th>
-                  <th style={{ width: '50px' }}>반</th>
-                  <th style={{ width: '50px' }}>번호</th>
-                  <th style={{ width: '100px' }}>이름</th>
+                  <th style={{borderTopLeftRadius: '5px', width: '120px'}}>권한</th>
+                  <th style={{width: '50px'}}>학년</th>
+                  <th style={{width: '50px'}}>반</th>
+                  <th style={{width: '50px'}}>번호</th>
+                  <th style={{width: '100px'}}>이름</th>
                   <th>전화번호</th>
                   <th>학부모1 전화번호</th>
                   <th>학부모2 전화번호</th>
-                  <th style={{ width: '70px' }}>아이디</th>
-                  <th style={{ borderTopRightRadius: '5px', width: '150px' }}>비밀번호</th>
+                  <th>아이디</th>
+                  <th style={{borderTopRightRadius: '5px'}}>비밀번호</th>
                 </tr>
                 </thead>
                 <tbody>
@@ -273,46 +266,66 @@ const AddUserWithTyping = () => {
                         </select>
                       </td>
 
-                      <td className="w-1/3">
-                        <input
-                          onChange={(e) =>
-                            modifyUserItem(index, e.target.value, 'grade')}
-                          value={newUserArray[index].grade}
-                          type={'number'}
-                          min={1}
-                          max={3}
-                          placeholder={'학년'}
-                          className={cs('number-input')}
-                        />
+                      <td>
+                        {
+                          newUserArray[index].position === 1 || newUserArray[index].position === 2 || newUserArray[index].position === 0 ?
+                            (<div className={cs('none-value-tag')}>-</div>) :
+                            (
+                              <input
+                                onChange={(e) =>
+                                  modifyUserItem(index, e.target.value, 'grade')}
+                                value={newUserArray[index].grade}
+                                type={'number'}
+                                min={1}
+                                max={3}
+                                placeholder={'학년'}
+                                className={cs('number-input')}
+                              />
+                            )
+                        }
                       </td>
 
-                      <td className="w-1/3">
-                        <input
-                          onChange={(e) =>
-                            modifyUserItem(index, e.target.value, 'class')}
-                          value={newUserArray[index].classNum}
-                          type={'number'}
-                          min={1}
-                          max={4}
-                          placeholder={'반'}
-                          className={cs('number-input')}
-                        />
+                      <td>
+                        {
+                          newUserArray[index].position === 1 || newUserArray[index].position === 2 || newUserArray[index].position === 0 ?
+                            (<div className={cs('none-value-tag')}>-</div>) :
+                            (
+                              <input
+                                onChange={(e) =>
+                                  modifyUserItem(index, e.target.value, 'class')}
+                                value={newUserArray[index].classNum}
+                                type={'number'}
+                                min={1}
+                                max={4}
+                                placeholder={'반'}
+                                className={cs('number-input')}
+                              />
+                            )
+                        }
+
                       </td>
 
-                      <td className="w-1/3">
-                        <input
-                          onChange={(e) =>
-                            modifyUserItem(index, e.target.value, 'number')}
-                          value={newUserArray[index].number}
-                          type={'number'}
-                          min={1}
-                          max={22}
-                          placeholder={'번호'}
-                          className={cs('number-input')}
-                        />
+                      <td>
+                        {
+                          newUserArray[index].position === 3 || newUserArray[index].position === 4 ?
+                            (
+                              <input
+                                onChange={(e) =>
+                                  modifyUserItem(index, e.target.value, 'number')}
+                                value={newUserArray[index].number}
+                                type={'number'}
+                                min={1}
+                                max={22}
+                                placeholder={'번호'}
+                                className={cs('number-input')}
+                              />
+                            ) :
+                            (<div className={cs('none-value-tag')}>-</div>)
+                        }
+
                       </td>
 
-                      <td className="w-1/3">
+                      <td>
                         <input
                           onChange={(e) =>
                             modifyUserItem(index, e.target.value, 'name')}
@@ -328,35 +341,49 @@ const AddUserWithTyping = () => {
                           onChange={(e) => modifyUserItem(index, e.target.value, 'studentPhone')}
                           value={newUserArray[index].phoneNum}
                           type={'tel'}
-                          placeholder={'학생 전화번호'}
+                          placeholder={'사용자 전화번호'}
                           maxLength={13}
                           className={cs('phone-number-input')}
                         />
                       </td>
 
-                      <td className="w-1/3">
-                        <input
-                          onChange={(e) => modifyUserItem(index, e.target.value, 'parentsPhone1')}
-                          value={newUserArray[index].parentsPhoneNum1}
-                          type={'tel'}
-                          placeholder={'학부모1 전화번호'}
-                          maxLength={13}
-                          className={cs('phone-number-input')}
-                        />
+                      <td>
+                        {
+                          newUserArray[index].position === 3 || newUserArray[index].position === 4 ?
+                            (
+                              <input
+                                onChange={(e) => modifyUserItem(index, e.target.value, 'parentsPhone1')}
+                                value={newUserArray[index].parentsPhoneNum1}
+                                type={'tel'}
+                                placeholder={'학부모1 전화번호'}
+                                maxLength={13}
+                                className={cs('phone-number-input')}
+                              />
+                            ) :
+                            (<div className={cs('none-value-tag')}>-</div>)
+                        }
+
                       </td>
 
-                      <td className="w-1/3">
-                        <input
-                          onChange={(e) => modifyUserItem(index, e.target.value, 'parentsPhone2')}
-                          value={newUserArray[index].parentsPhoneNum2}
-                          type={'tel'}
-                          placeholder={'학부모2 전화번호'}
-                          maxLength={13}
-                          className={cs('phone-number-input')}
-                        />
+                      <td>
+                        {
+                          newUserArray[index].position === 3 || newUserArray[index].position === 4 ?
+                            (
+                              <input
+                                onChange={(e) => modifyUserItem(index, e.target.value, 'parentsPhone2')}
+                                value={newUserArray[index].parentsPhoneNum2}
+                                type={'tel'}
+                                placeholder={'학부모2 전화번호'}
+                                maxLength={13}
+                                className={cs('phone-number-input')}
+                              />
+                            ) :
+                            (<div className={cs('none-value-tag')}>-</div>)
+                        }
+
                       </td>
 
-                      <td className="w-1/3">
+                      <td>
                         <input
                           onChange={(e) =>
                             modifyUserItem(index, e.target.value, 'account')}
@@ -366,17 +393,18 @@ const AddUserWithTyping = () => {
                           placeholder={'아이디 / 학번'}
                         />
                       </td>
-                      <td className="w-1/3">
+
+                      <td>
                         <div>
-                        <input
-                          onChange={(e) =>
-                            modifyUserItem(index, e.target.value, 'password')}
-                          value={newUserArray[index].password}
-                          type={newUserArray[index].showPassword ? "text" : "password"}
-                          maxLength={15}
-                          placeholder={'4자리 이상 입력'}
-                          className={cs('password-input')}
-                        />
+                          <input
+                            onChange={(e) =>
+                              modifyUserItem(index, e.target.value, 'password')}
+                            value={newUserArray[index].password}
+                            type={showPassword ? "text" : "password"}
+                            maxLength={15}
+                            placeholder={'4자리 이상 입력'}
+                            className={cs('password-input')}
+                          />
                         </div>
                       </td>
                     </motion.tr>
@@ -394,6 +422,16 @@ const AddUserWithTyping = () => {
               <CgPlayListAdd className={cs('list-icon')}/> <span>행 추가</span></button>
             <button onClick={removeUser} className={cs('remove-row-button')}>
               <CgPlayListRemove className={cs('list-icon')}/> <span>행 제거</span></button>
+            {
+              newUserArray.length > 0 &&
+              (
+                <button onClick={handleShowPassword} className={cs('show-password-button')}>
+                  {
+                    showPassword ? (<div><AiFillEyeInvisible className={cs('eye-icon')}/> <span>비밀번호 숨김</span></div>) :
+                      (<div><AiFillEye className={cs('eye-icon')}/> <span>비밀번호 보기</span></div>)
+                  }
+                </button>
+              )}
 
             <button onClick={AddUserHandler} className={cs('add-user-button')}>
               <FiUserPlus className={cs('add-user-icon2')}/> <span>사용자 추가</span></button>
