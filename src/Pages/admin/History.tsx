8580a843d2017213d87e3ef2a744da973d494972
jinhyo-dev <jinhyo0850@gmail.com>
2022-12-17 20:24:05 +0900

    DatePicker added

--- src/Pages/admin/History.tsx
+++ src/Pages/admin/History.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { forwardRef, useEffect, useState } from 'react'
 import AdminSideBar from "../../Components/Sidebar/AdminSideBar"
 import { AiOutlineHome } from "react-icons/ai"
 import LogoutButton from "../../Components/LogoutButton"
@@ -7,7 +7,10 @@ import classNames from "classnames/bind"
 import styles from '../../Style/History.module.css'
 import { BsArrowUp, BsArrowDown } from 'react-icons/bs'
 import useThrottle from '../../utils/useThrottle'
-import ReactTooltip from 'react-tooltip'
+import DatePicker from 'react-datepicker'
+import "react-datepicker/dist/react-datepicker.css";
+import { ko } from 'date-fns/esm/locale'
+import { AiOutlineCalendar } from 'react-icons/ai'
 
 const cs = classNames.bind(styles)
 
@@ -61,6 +64,15 @@ const History = () => {
     )
   }
 
+  const date = new Date()
+
+  const year = date.getFullYear()
+  const month = date.getMonth()
+  const day = date.getDate()
+
+  const [startDate, setStartDate] = useState<Date>(new Date(year, month, day - 7));
+  const [endDate, setEndDate] = useState<Date>(date);
+
   const [data, setData] = useState<dataValue[]>([])
 
   const [headContent, setHeadContent] = useState([
@@ -87,6 +99,13 @@ const History = () => {
         setData(data.map((v: any) => ({...v, isChecked: false})))
       })
   }, [])
+
+  const datePickerCustom = ({ value, onClick }: {value: string; onClick: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void}) => (
+    <button className="date-picker-custom" onClick={onClick}>
+      <AiOutlineCalendar className={'calendar-icon'}/>
+      {value}
+    </button>
+  )
 
   const checkAllButton = (isClicked: boolean) => {
     setData(data.map((v: dataValue) =>
@@ -118,17 +137,17 @@ const History = () => {
     } else if (title === 'number') {
       data.sort((a: dataValue, b: dataValue) => asc ? (a.number < b.number) ? 1 : -1 : (a.number > b.number) ? 1 : -1)
     } else if (title === 'name') {
-      data.sort((a: dataValue, b: dataValue) => asc ? (a.name < b.name) ? 1 : -1 : (a.name > b.name) ? 1 : -1)
+      data.sort((a: dataValue, b: dataValue) => asc ? (a.name > b.name) ? 1 : -1 : (a.name < b.name) ? 1 : -1)
     } else if (title === 'division') {
       data.sort((a: dataValue, b: dataValue) => asc ? (a.division < b.division) ? 1 : -1 : (a.division > b.division) ? 1 : -1)
     } else if (title === 'regulate') {
-      data.sort((a: dataValue, b: dataValue) => asc ? (a.regulate < b.regulate) ? 1 : -1 : (a.regulate > b.regulate) ? 1 : -1)
+      data.sort((a: dataValue, b: dataValue) => asc ? (a.regulate > b.regulate) ? 1 : -1 : (a.regulate < b.regulate) ? 1 : -1)
     } else if (title === 'score') {
       data.sort((a: dataValue, b: dataValue) => asc ? (a.score < b.score) ? 1 : -1 : (a.score > b.score) ? 1 : -1)
     } else if (title === 'issuer') {
       data.sort((a: dataValue, b: dataValue) => asc ? (a.issuer < b.issuer) ? 1 : -1 : (a.issuer > b.issuer) ? 1 : -1)
     } else if (title === 'date') {
-      data.sort((a: dataValue, b: dataValue) => asc ? (a.createdDate < b.createdDate) ? 1 : -1 : (a.createdDate > b.createdDate) ? 1 : -1)
+      data.sort((a: dataValue, b: dataValue) => asc ? (a.createdDate > b.createdDate) ? 1 : -1 : (a.createdDate < b.createdDate) ? 1 : -1)
     }
 
     setData([...data])
@@ -136,16 +155,6 @@ const History = () => {
     console.log(headContent)
   }
 
-  const sortArray = (sortDivision: string) => {
-    if (sortDivision === 'desc') {
-      data.sort((a: any, b: any) => (a.createdDate < b.createdDate) ? 1 : -1)
-    } else if (sortDivision === 'asc') {
-      data.sort((a: any, b: any) => (a.createdDate > b.createdDate) ? 1 : -1)
-    }
-
-    setData([...data])
-    console.log(data)
-  }
   const editHistory = (id: number) => {
     for (let i = 0; i < data.length; i++) {
       if (data[i].id === id) {
@@ -167,44 +176,133 @@ const History = () => {
         </div>
 
         <div className={cs('search-bar')}>
-          <input onChange={(e) => setText(e.target.value)}/>
-          <div>{throttledText}</div>
+          <div className={'date-picker-container'}>
+
+            <DatePicker
+              locale={ko}
+              dateFormat={'yyyy-MM-dd'}
+              selected={startDate}
+              onChange={(date: Date) => setStartDate(date)}
+              startDate={startDate}
+              customInput={React.createElement(datePickerCustom)}
+              endDate={endDate}
+              maxDate={date}
+            />
+            <div className={'date-hyphen'}>-</div>
+            <DatePicker
+              locale={ko}
+              dateFormat={'yyyy-MM-dd'}
+              selected={endDate}
+              onChange={(date: Date) => setEndDate(date)}
+              startDate={startDate}
+              endDate={endDate}
+              customInput={React.createElement(datePickerCustom)}
+              minDate={startDate}
+              maxDate={date}
+            />
+
+            <div className={'search-btn'}>
+              <button>조회</button>
+            </div>
+
+          </div>
+
+
+
+          {/*<input onChange={(e) => setText(e.target.value)}/>*/}
+          {/*<div>{throttledText}</div>*/}
         </div>
-        <div className={'container'}>
+
+        <div className={'container'} style={{ marginTop: '1vh' }}>
           <div className={'management-table-container'}>
             <table>
               <thead>
               <th><input type={"checkbox"} onClick={(e) => checkAllButton(e.currentTarget.checked)}/></th>
 
               <th
-                  className={'number-value'}
-                  onMouseOver={() => handleMouseOver('grade', true)}
-                  onMouseLeave={() => handleMouseOver('grade', false)}>
+                className={'number-value'}
+                onMouseOver={() => handleMouseOver('grade', true)}
+                onMouseLeave={() => handleMouseOver('grade', false)}>
                 {
-                  headContent[0].isHover ? (headContent[0].order ? arrowDownButton('grade') : arrowUpButton('grade')) : (<span>학년</span>)
+                  headContent[0].isHover ? (headContent[0].order ? arrowDownButton('grade') : arrowUpButton('grade')) : (
+                    <span>학년</span>)
                 }
               </th>
+
               <th
-                  className={'number-value'}
-                  onMouseOver={() => handleMouseOver('class', true)}
-                  onMouseLeave={() => handleMouseOver('class', false)}>
+                className={'number-value'}
+                onMouseOver={() => handleMouseOver('class', true)}
+                onMouseLeave={() => handleMouseOver('class', false)}>
                 {
-                  headContent[1].isHover ? (headContent[1].order ? arrowDownButton('class') : arrowUpButton('class')) : (<span>반</span>)
+                  headContent[1].isHover ? (headContent[1].order ? arrowDownButton('class') : arrowUpButton('class')) : (
+                    <span>반</span>)
                 }
               </th>
-              <th className={'number-value'}
-              >번호</th>
-              <th>이름</th>
-              <th>구분</th>
-              <th>발급항목</th>
+
+              <th
+                className={'number-value'}
+                onMouseOver={() => handleMouseOver('number', true)}
+                onMouseLeave={() => handleMouseOver('number', false)}>
+                {
+                  headContent[2].isHover ? (headContent[2].order ? arrowDownButton('number') : arrowUpButton('number')) : (
+                    <span>번호</span>)
+                }
+              </th>
+
+              <th
+                onMouseOver={() => handleMouseOver('name', true)}
+                onMouseLeave={() => handleMouseOver('name', false)}>
+                {
+                  headContent[3].isHover ? (headContent[3].order ? arrowDownButton('name') : arrowUpButton('name')) : (
+                    <span>이름</span>)
+                }
+              </th>
+
+              <th
+                onMouseOver={() => handleMouseOver('division', true)}
+                onMouseLeave={() => handleMouseOver('division', false)}>
+                {
+                  headContent[4].isHover ? (headContent[4].order ? arrowDownButton('division') : arrowUpButton('division')) : (
+                    <span>구분</span>)
+                }
+              </th>
+
+              <th
+                onMouseOver={() => handleMouseOver('regulate', true)}
+                onMouseLeave={() => handleMouseOver('regulate', false)}>
+                {
+                  headContent[5].isHover ? (headContent[5].order ? arrowDownButton('regulate') : arrowUpButton('regulate')) : (
+                    <span>발급항목</span>)
+                }
+              </th>
+
               <th onMouseOver={() => handleMouseOver('score', true)}
                   onMouseLeave={() => handleMouseOver('score', false)}>
                 {
-                  headContent[6].isHover ? (headContent[6].order ? arrowDownButton('score') : arrowUpButton('score')) : (<span>점수</span>)
+                  headContent[6].isHover ? (headContent[6].order ? arrowDownButton('score') : arrowUpButton('score')) : (
+                    <span>점수</span>)
                 }
               </th>
-              <th>발급자</th>
-              <th>발급일</th>
+
+              <th
+                style={{minWidth: '45px'}}
+                onMouseOver={() => handleMouseOver('issuer', true)}
+                onMouseLeave={() => handleMouseOver('issuer', false)}>
+                {
+                  headContent[7].isHover ? (headContent[7].order ? arrowDownButton('issuer') : arrowUpButton('issuer')) : (
+                    <span>발급자</span>)
+                }
+              </th>
+
+              <th
+                onMouseOver={() => handleMouseOver('date', true)}
+                onMouseLeave={() => handleMouseOver('date', false)}>
+                {
+                  headContent[8].isHover ? (headContent[8].order ? arrowDownButton('date') : arrowUpButton('date')) : (
+                    <span>발급일</span>)
+                }
+              </th>
+
               </thead>
 
               <tbody>
